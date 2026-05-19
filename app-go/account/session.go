c8ca9f58e4a5f5250ffaf7a9bca3d0b5b49de240
hayokbt <mihokubota@outlook.jp>
2026-05-19 21:38:46 +0900

    Allow DELETE method in CORS preflight response

--- app-go/account/session.go
+++ app-go/account/session.go
@@ -30,7 +30,7 @@ func handleCorsPreflight(w http.ResponseWriter, r *http.Request) bool {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusNoContent)
 		return true
